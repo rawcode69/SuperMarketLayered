f37a5558c9a1793bff05fff47a11a143a8d4d8e1
Ayeshmantha <ravindu.ayesh123@gmail.com>
2023-08-13 17:00:29 +0530

    View Layer Created Completing UI

--- src/supermarketlayered/SupermarketLayered.java
+++ src/supermarketlayered/SupermarketLayered.java
@@ -4,6 +4,8 @@
  */
 package supermarketlayered;
 
+import supermarketlayered.view.LayoutView;
+
 /**
  *
  * @author Ravidu Ayeshmanth
@@ -14,7 +16,7 @@ public class SupermarketLayered {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new LayoutView().setVisible(true);
     }
     
 }
